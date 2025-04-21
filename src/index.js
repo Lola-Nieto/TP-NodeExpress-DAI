ab2e425150516ed24c65d4bd61e96c86c2b04952
Android Studio <devandroid@ort>
2025-04-21 09:19:14 -0300

    9:15 21/04

--- src/index.js
+++ src/index.js
@@ -12,27 +12,83 @@ const port = 3000; // El puerto 3000 (http://localhost:3000)
 app.use(cors()); // Middleware de CORS
 app.use(express.json()); // Middleware para parsear y comprender JSON
 
+
+
+
+// Inicio el Server y lo pongo a escuchar.
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    })
+    
+
 // Aca pongo todos los EndPoints
+
+//Comunes
+
 app.get('/', (req, res) => { // EndPoint "/"
     res.send('¡Ya estoy respondiendo!');
     //res.status(200).json({'OK'});
 })
 app.get('/saludar/:nombre', (req, res) => { // EndPoint "/saludar"
     res.send(`Hola ${req.params.nombre}`);
-    res.sendStatus(200); //Sale el estado y un mensaje (OK?)
+    res.sendStatus(200); //No sale nada
 })
+
+
 app.get('/validarfecha/:anio/:mes/:dia', (req, res) => { // EndPoint "/validarfecha"
-    
-    if(Date.parse(req.params.mes + req.params.dia + req.params.anio) != isNaN){
-        res.sendStatus(200); //Sale el estado y un mensaje (OK?)
-    }else{
+
+    if(Number.isNaN(Date.parse(new Date(req.params.mes, req.params.dia, req.params.anio)))){
         res.sendStatus(400);
+    }else{
+        res.sendStatus(200);
+    }
+
+})//Valida que sean números, nada más (se puede poner como mes 13 y tira OK)
+    
+
+//Matemática
+
+app.get('/matematica/sumar', (req, res) => {
+    let n1 =  req.query.n1;
+    let n2 =  req.query.n2;
+
+    let resultado = sumar(n1, n2);
+    res.send('Resultado: '+ resultado);
+
+    if(isNaN(resultado)){
+        res.sendStatus(400);
+    }else{
+        res.sendStatus(200);
+
     }
 
 })
-    
 
-// Inicio el Server y lo pongo a escuchar.
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
+
+app.get('/matematica/restar', (req, res) => {
+    let n1 =  req.query.n1;
+    let n2 =  req.query.n2;
+    res.send('Resultado: '+ restar(n1, n2));
+    res.sendStatus(200);
+})
+
+app.get('/matematica/multiplicar', (req, res) => {
+    let n1 =  req.query.n1;
+    let n2 =  req.query.n2;
+    res.send('Resultado: '+ multiplicar(n1, n2));
+    res.sendStatus(200);
+})
+
+app.get('/matematica/dividir', (req, res) => {
+    let n1 =  req.query.n1;
+    let n2 =  req.query.n2;
+    let resultado = dividir(n1, n2);
+    res.send('Resultado: '+ resultado);
+    if(isNaN(resultado)){
+        res.sendStatus(400);
+    }else{
+        res.sendStatus(200);
+
+    }
+
 })
--- src/modules/matematica.js
+++ src/modules/matematica.js
@@ -1,21 +1,57 @@
 const PI = 3.14;
 const arrayNums = ["dos", "cuatro", "ocho", "diez"];
 function sumar(x, y) {
-    return (x+y); 
+    const sonNums = ValidarSiSonNumeros(x, y);
+    if(sonNums == true)
+        return (parseInt(x)+parseInt(y)); 
+    else{
+        return sonNums; 
+    }
 }
 const restar = (x, y) => {
-    return (x-y);
+    const sonNums = ValidarSiSonNumeros(x, y);
+
+    if(sonNums ==true)
+        return (x-y);
+    else{
+        return sonNums; 
+    }
+    
 }
 const multiplicar = (a, b) => {
-return (a*b)
+ 
+const sonNums = ValidarSiSonNumeros(a, b);
+
+if(sonNums ==true)
+    return (a*b);
+else{
+    return sonNums; 
+}
+
 };
 function dividir(x, y) {
-    let ret;
-    if(y != 0){
-        ret = x/y;
-    }else{
-        ret = "No se puede dividir por 0";
-    }  
+
+const sonNums = ValidarSiSonNumeros(x, y);
+let ret;
+
+if(sonNums ==true)
+if(y != 0){
+    ret = x/y;
+}else{
+    ret = "No se puede dividir por 0";
+}  
+else{
+    ret = sonNums; 
+}
     return ret;
 }
+
+function ValidarSiSonNumeros(x, y){
+    let ret = true;
+    if(isNaN(x) || isNaN(y))
+    ret = "Error, los ingresos no son números";
+
+    return ret;
+}
+
 export {PI, sumar, multiplicar, restar, arrayNums, dividir};
